23bee57384eec865e21a45f7ff6886ceb6003808
Ben Scheirman <ben@scheirman.com>
2011-05-31 14:28:45 -0500

    Added path helpers for getting files in the docs directory

---
+++ CHPathHelpers.h
@@ -0,0 +1,13 @@
+//
+//  CHPathHelpers.h
+//  ChaiOneUtils
+//
+//  Created by Ben Scheirman on 5/31/11.
+//  Copyright 2011 ChaiONE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NSString *PathForFileInDocumentsDirectory(NSString *filepath);
+NSString *DocumentsDirectory();
---
+++ CHPathHelpers.m
@@ -0,0 +1,26 @@
+//
+//  CHPathHelpers.m
+//  ChaiOneUtils
+//
+//  Created by Ben Scheirman on 5/31/11.
+//  Copyright 2011 ChaiONE. All rights reserved.
+//
+
+#import "CHPathHelpers.h"
+
+NSString *PathForFileInDocumentsDirectory(NSString *filepath) {
+    return [DocumentsDirectory() stringByAppendingPathComponent:filepath];
+}
+
+NSString * DocumentsDirectory() {
+    NSString *docsDir = nil;
+    NSArray *directories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    if ([directories count] > 0)
+        docsDir = [directories objectAtIndex:0];
+    
+    if(!docsDir) {
+        [NSException raise:@"Critical Error" format:@"Can't find documents directory!"];
+    }
+    
+    return docsDir;
+}
