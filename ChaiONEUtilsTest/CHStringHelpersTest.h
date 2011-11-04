97293092f365d6e734d970344810a156e552ef9b
Longyi Qi <longyi@chaione.com>
2011-11-04 14:52:22 -0500

    tests for string helpers

---
+++ ChaiONEUtilsTest/CHStringHelpersTest.h
@@ -0,0 +1,13 @@
+//
+//  CHStringHelpersTest.h
+//  ChaiOneUtils
+//
+//  Created by Longyi Qi on 11/4/11.
+//  Copyright (c) 2011 ChaiONE Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CHStringHelpersTest : SenTestCase
+
+@end
