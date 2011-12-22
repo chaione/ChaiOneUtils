df660ed248b7aa703c93486fef2539a6ec44d84e
Ben Scheirman <ben@scheirman.com>
2011-12-22 11:00:32 -0600

    Build for i386 and armv6/7
    Updated build script to run simulator and iphoneos builds

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+set -e
+
+echo "Building for iPhone Simulator 5.0..."
 xcodebuild -project ChaiOneUtils.xcodeproj -target ChaiOneUtils -configuration Debug -sdk iphonesimulator5.0 clean build
+
+echo "Building for iPhone Device 5.0..."
+xcodebuild -project ChaiOneUtils.xcodeproj -target ChaiOneUtils -configuration Debug -sdk iphoneos5.0 clean build
